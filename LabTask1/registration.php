57b59cae710e4222054b14dc426118b65cf46421
S.M.IRAMUL MURSHED <35691070+iramulmurshed@users.noreply.github.com>
2021-02-07 15:06:36 +0600

    LabTask1

---
+++ LabTask1/registration.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Registration Page </title>
+</head>
+</body>
+<h1>My Registration Page</h1>
+
+<form>
+<table>
+<tr> 
+<td>Name:</td>
+<td><input type="text" name="firstname"></td></tr>
+
+
+<tr>
+<td>Email:</td>
+<td><input type="text" name="email"></td></tr>
+
+<tr>
+<td>User Name:</td>
+<td><input type="text" name="User Name"></td></tr>
+
+<tr>
+<td>Password:</td>
+<td><input type="password" name="Password"></td></tr>
+
+<tr>
+<td>Confirm Password:</td>
+<td><input type="password" name="Confirm Password"></td></tr>
+
+<td>Gender</td>
+</tr>
+<td><input type="radio" name="Male">Male</td>
+<td><input type="radio" name="Female">Female</td>
+<td><input type="radio" name="Other">Other</td></tr>
+<br>
+<br>
+<td>Date of Birth</td>
+<td> <input type="date" id="birthday" name="birthday"> <td>
+<br>
+
+<tr>
+<td><input type="submit" value="submit"> <input type="reset" value="reset"> </form><td></tr>
+
+</table>
+</form>
+
+
+</body>
+</html>
